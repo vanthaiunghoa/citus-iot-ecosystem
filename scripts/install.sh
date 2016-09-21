bae0447223c20c3749378a00c8f4d865fdbe4fd6
CuongQuay <cuong3ihut@gmail.com>
2016-09-21 14:30:07 +0700

    Update bootstrap scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,20 +10,17 @@ cd $HOME/citus-iot-device-agents/ && sudo bash install.sh
 cd $HOME; rm -rf $HOME/citus-iot-device-agents/
 echo "Installed Software Agents successfully!"
 
-echo "Do you wish to install Citus IoT Device SDK for Javascript?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes )
-        	echo "Installing NodeJS 6.x..."
-			curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-			sudo apt install -y nodejs
-			echo "Installing Citus IoT Device SDK..." 
-			https://github.com/cuongquay/citus-iot-sdk-nodejs.git $HOME/citus-iot-sdk-nodejs/
-			cd $HOME/citus-iot-sdk-nodejs/ && npm install && npm update
-			echo "Try it out! Type 'npm run examples' on the console."
-			cd $HOME; rm -rf $HOME/citus-iot-sdk-nodejs/
-			echo "Installed SDK successfully!" 
-			break;;
-        No ) exit;;
-    esac
-done
+while read -r -t 0; do read -r; done
+read -r -p "Do you wish to install Citus IoT Device SDK for Javascript [Y/n]?" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]] | [ -z $response ]; then
+    echo "Installing NodeJS 6.x..."
+	curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+	sudo apt install -y nodejs
+	echo "Installing Citus IoT Device SDK..." 
+	https://github.com/cuongquay/citus-iot-sdk-nodejs.git $HOME/citus-iot-sdk-nodejs/
+	cd $HOME/citus-iot-sdk-nodejs/ && npm install && npm update
+	echo "Try it out! Type 'npm run examples' on the console."
+	cd $HOME; rm -rf $HOME/citus-iot-sdk-nodejs/
+	echo "Installed SDK successfully!"
+fi
