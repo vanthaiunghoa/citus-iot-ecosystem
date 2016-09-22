023d80ae6e7e00a5f560e86d0ec36e0824f48191
CuongQuay <cuong3ihut@gmail.com>
2016-09-22 11:50:43 +0700

    Update bootstrap

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,4 +8,7 @@ echo "Installing Software Agents..."
 git clone https://github.com/cuongquay/citus-iot-device-agents.git $HOME/citus-iot-device-agents/
 cd $HOME/citus-iot-device-agents/ && sudo bash install.sh
 cd $HOME; rm -rf $HOME/citus-iot-device-agents/
+mkdir -p $HOME/.agent/
+echo "${DEVICE_ID}" >> ${HOME}/.agent/.device-id
+echo "${SECRET_KEY}" >> ${HOME}/.agent/.secret-key
 echo "Installed Software Agents successfully!"
