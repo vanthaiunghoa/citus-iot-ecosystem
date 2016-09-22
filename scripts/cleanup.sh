e1e42413be6367d448ef1277dc76e62df4759206
CuongQuay <cuong3ihut@gmail.com>
2016-09-22 16:16:11 +0700

    Update agent installer

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
-echo "Are you sure you want to destroy this device?"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+NC='\033[1;30m' # No Color
+
+echo -e "${RED}DANGEROUS: Are you sure you want to destroy this device?${NC}"
 select yn in "Yes" "No"; do
     case $yn in
         Yes )
-        	echo "Deactivating this device against platform..."
+        	echo -e "${BLUE}INFO: Deactivating this device against platform...${NC}"
 			sudo citus-device revoke
-			echo "Removing device certificates and agents..." 
+			echo -e "${BLUE}INFO: Removing device certificates and agents...${NC}" 
 			sudo citus-device remove
-			echo "Cleanup device successfully!" 
+			echo -e "${BLUE}INFO: Cleanup device successfully!${NC}" 
 			break;;
         No ) exit;;
     esac
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[1;34m'
-NC='\033[0m' # No Color
+NC='\033[1;30m' # No Color
 
 echo -e "${GREEN}INFO: Updating OS software packages...${NC}"
 sudo apt update
