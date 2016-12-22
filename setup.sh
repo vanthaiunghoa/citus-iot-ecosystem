3fd08621c74d61d51f3bb37bc53f4e195c19f3ed
CuongQuay <cuong3ihut@gmail.com>
2016-12-22 09:36:59 +0700

    Update Amazon AMI compatible

--- setup.sh
+++ setup.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 set -e -x 
 
-tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF
-yum install docker-engine -y
-systemctl enable docker.service
-systemctl start docker
-usermod -aG docker centos
+if [ `cat /etc/os-release | grep -q "Amazon Linux AMI"` ]; then
+	yum install docker-engine -y
+	chkconfig --add docker
+	chkconfig docker on
+	service docker start
+	usermod -aG docker ec2-user	
+else	
+	tee /etc/yum.repos.d/docker.repo <<-'EOF'
+	[dockerrepo]
+	name=Docker Repository
+	baseurl=https://yum.dockerproject.org/repo/main/centos/7/
+	enabled=1
+	gpgcheck=1
+	gpgkey=https://yum.dockerproject.org/gpg
+	EOF
+	yum install docker-engine -y
+	systemctl enable docker.service
+	systemctl start docker
+	usermod -aG docker centos	
+	setsebool httpd_can_network_connect on -P
+	getsebool -a | grep httpd
+	yum clean all
+fi
+
 curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
-setsebool httpd_can_network_connect on -P
-getsebool -a | grep httpd
-yum clean all && cd /usr/share/citus-iot-ecosystem/
+cd /usr/share/citus-iot-ecosystem/
 docker-compose up -d --force-recreate
