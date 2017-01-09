8940deeea8c3132a43599cbdd305fa2f1675daf0
CuongQuay <cuong3ihut@gmail.com>
2017-01-09 11:15:30 +0700

    Fix undefined app name

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ set -e -x
 OS_RELEASE=$(cat /etc/os-release)
 AMAZON_AMI="Amazon Linux AMI"
 CENTOS_AMI="CentOS Linux"
+KUBE_AWS_VERSION="0.9.3-rc.2"
 
 if [ -z "${OS_RELEASE##*$AMAZON_AMI*}" ]; then
 	yum install docker -y
@@ -19,7 +20,11 @@ else
 	gpgcheck=1
 	gpgkey=https://yum.dockerproject.org/gpg
 	EOF
-	yum install docker-engine -y
+	yum install docker-engine python27 -y
+	curl -O https://bootstrap.pypa.io/get-pip.py
+	python27 get-pip.py	
+	pip install awscli && \	
+	pip install --upgrade awscli && \
 	systemctl enable docker.service
 	systemctl start docker
 	usermod -aG docker centos	
@@ -28,6 +33,14 @@ else
 	yum clean all
 fi
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
+chmod +x ./kubectl && \
+mv ./kubectl /usr/local/bin/kubectl && \    
+curl -L https://github.com/coreos/kube-aws/releases/download/v${KUBE_AWS_VERSION}/kube-aws-linux-amd64.tar.gz -o /tmp/kube-aws-linux-amd64.tar.gz && \	
+tar -zxvf /tmp/kube-aws-linux-amd64.tar.gz && \
+mv linux-amd64/kube-aws /usr/local/bin && \
+rm -f /tmp/kube-aws-linux-amd64.tar.gz
+    
 curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
