f2692912eaeca10aa80b546ac9257636d4b78f10
CuongQuay <cuong3ihut@gmail.com>
2017-03-08 08:56:31 +0700

    Add bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e -x
 
+export AWS_DEFAULT_REGION=ap-northeast-1
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_IOT_CERT_BUCKET=apps.citus.io/certs
+export AWS_IOT_DEVICE_POLICY=CitusSensorPolicy
+export AWS_METADATA_TABLE_NAME=citus-iot-ecosystem.metadata
+export AWS_SENSORS_TABLE_NAME=citus-iot-ecosystem.sensors
+export AWS_SENSORS_STREAM_VIEW_TYPE=new_and_old_images
+
 cd /usr/share/citus-iot-ecosystem/production/
 docker-compose down
 docker rmi $(docker images -q)
