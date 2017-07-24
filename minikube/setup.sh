af31a1f91c2543208a7b929571ff9ab7c470e67f
CuongQuay <cuong3ihut@gmail.com>
2017-07-24 14:21:15 +0700

    Update minikube version

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -44,6 +44,7 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBERNETES_
 chmod +x ./kubectl && mv ./kubectl /usr/bin/kubectl
 
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+ln -s /usr/local/bin/minikube /usr/bin/minikube
 
 minikube start
 
