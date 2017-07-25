9087a1eb016b18dbb68bf081f808a386c7c0df6d
CuongQuay <cuong3ihut@gmail.com>
2017-07-25 10:12:01 +0700

    Correct migration script

--- minikube/setup.sh
+++ minikube/setup.sh
@@ -43,6 +43,7 @@ users:
     
 EOF
 
+kubectl apply -f default-services/kubernetes-dashboard.yaml
 kubectl apply -f default-services/citus-iot-ecosystem.yaml
 sh default-services/citus-iot-ecosystem.sh
 sh default-services/citus-cassandra-database.sh
