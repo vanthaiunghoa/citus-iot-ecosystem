8147e15229b177f51b444ca645b3fc2014055da9
CuongQuay <cuong3ihut@gmail.com>
2017-07-25 14:20:21 +0700

    Correct migration script

--- minikube/default-services/citus-iot-ecosystem.sh
+++ minikube/default-services/citus-iot-ecosystem.sh
@@ -104,7 +104,7 @@ spec:
         - containerPort: 3000
         env:
         - name: AWS_DEFAULT_REGION
-          value: us-east-1
+          value: ${AWS_DEFAULT_REGION}
         - name: AWS_ACCESS_KEY_ID
           value: ${AWS_ACCESS_KEY_ID}
         - name: AWS_SECRET_ACCESS_KEY
